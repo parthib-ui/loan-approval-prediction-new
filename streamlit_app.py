af22f3675dd0a6d6a47a65030edbfb255cf2cf32
parthib-ui <146550465+parthib-ui@users.noreply.github.com>
2025-07-25 19:49:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@
 import pandas as pd
 import numpy as np
 
-import seaborn as sns
+
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
