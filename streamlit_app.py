fe9dd9d413892e6968f37589a3dc608af10c7b11
parthib-ui <146550465+parthib-ui@users.noreply.github.com>
2025-07-25 19:50:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 
-from sklearn.model_selection import train_test_split
+
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.neighbors import KNeighborsClassifier
